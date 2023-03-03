7d1defbf162edafb7f6369bd077ac7e847999d53
johnnam1121 <johnnam93@gmail.com>
2023-03-03 09:12:39 -0600

    created boilerplate

---
+++ src/components/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>Hello World, my name is Diana Aragon!</h1>
+      <h1>Hello World, my name is Diana Aragon!</h1>
+      <h1>Hello World, my name is Diana Aragon!</h1>
+      <h1>Hello World, my name is Diana Aragon!</h1>
+    </div>
+  )
+}
+
+export default LandingPage
